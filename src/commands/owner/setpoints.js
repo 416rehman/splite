22e4dcb534c437503dfa7e334650617030041337
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-21 01:40:57 -0400

    added point validation to setpoints.js

--- src/commands/owner/setpoints.js
+++ src/commands/owner/setpoints.js
@@ -1,5 +1,4 @@
 const Command = require('../Command.js');
-const {isNumber} = require("node-os-utils");
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class WipePointsCommand extends Command {
@@ -18,7 +17,7 @@ module.exports = class WipePointsCommand extends Command {
     const member =  this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
     if (!member) 
       return this.sendErrorMessage(message, 0, 'Please mention a user or provide a valid user ID');
-    if (!isNumber(args[1]))
+    if (isNaN(args[1]))
       return this.sendErrorMessage(message, 0, 'Please provide the amount of points to set');
     message.client.db.users.setPoints.run(args[1], member.id, message.guild.id);
     const embed = new MessageEmbed()
