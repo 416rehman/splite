1e20bd8f4540b2c63f19f5de6332cebb99c37be5
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-21 01:42:43 -0400

    fixed typo in anonymous.js

--- src/slashCommands/anonymous.js
+++ src/slashCommands/anonymous.js
@@ -23,17 +23,17 @@ module.exports = {
 
     anonymous: function anonymous(interaction, client) {
         const prefix = (client.db.settings.selectPrefix.pluck().get(interaction.guild_id))
-        const points = (client.db.users.selectPoints.pluck().get(interaction.member.user.id, interaction.guild_id))
-        if (!points || points < cost)
-        {
-            reply(interaction, `**You need ${cost-points} more points to send an anonymous message in this server.**\n\nEarn points by sending messages, talking in VC, and being active.\nTo check your points, type \`${prefix}points\``, client)
-        }
         const anonymousAllowed = (client.db.settings.selectAnonymous.pluck().get(interaction.guild_id))
         const anonMsg = interaction.data.options[0].value;
         console.log( {anonymousAllowed})
         if (!anonymousAllowed) {
-            reply(interaction, `This server doesn't allow anonymous messages. An admin can change this by typing \`${prefix}setanonymous\``, client)
+            reply(interaction, `This server doesn't allow anonymous messages. An admin can change this by typing \`${prefix}toggleanonymous\``, client)
         } else {
+            const points = (client.db.users.selectPoints.pluck().get(interaction.member.user.id, interaction.guild_id))
+            if (!points || points < cost)
+            {
+                reply(interaction, `**You need ${cost-points} more points to send an anonymous message in this server.**\n\nEarn points by sending messages, talking in VC, and being active.\nTo check your points, type \`${prefix}points\``, client)
+            }
             const embed = new MessageEmbed()
                 .setTitle(`Anonymous Message`)
                 .setDescription(`"${anonMsg}"`)
