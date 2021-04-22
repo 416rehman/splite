fa0b39ceeb4a4c461980861302e7300233e0c8b4
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-22 00:00:13 -0400

    View confessions roles

--- src/slashCommands/view.js
+++ src/slashCommands/view.js
@@ -34,7 +34,9 @@ module.exports = {
             if (!user.roles.cache.has(role.id))
                 reply(interaction, `**You don't have perms to run this command**`, client)
             else {
-                const { confession_id: confession_id, content : content, author_id : author_id, timeanddate : timeanddate, guild_id : guild_id } = client.db.confessions.selectConfessionByID.get(interaction.data.options[0].value)
+                //const { confession_id: confession_id, content : content, author_id : author_id, timeanddate : timeanddate, guild_id : guild_id } = client.db.confessions.selectConfessionByID.get(interaction.data.options[0].value)
+                const row = client.db.confessions.selectConfessionByID.get(interaction.data.options[0].value)
+
                 if (confession_id && guild_id === interaction.guild_id)
                 {
                     const sender = guild.members.cache.get(author_id);
