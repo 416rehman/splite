e1a98b6df116e91debe3524a8e99429ffcb6280c
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-21 23:25:07 -0400

    View confessions roles

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -144,7 +144,7 @@ function callSlashCommand(command, client, interaction){
   if (command === 'confess') confessions.confess(interaction, client);
   if (command === 'report') report.report(interaction, client);
   if (command === 'anonymous') anonymous.anonymous(interaction, client);
-  if (command === 'view') anonymous.anonymous(interaction, client);
+  if (command === 'view') view.view(interaction, client);
 }
 
 function getEmojiForJoinVoting(guild, client) {
