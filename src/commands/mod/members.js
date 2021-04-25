3ab6d51f41adaaba06c496a0bc89d02cfd285130
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-25 15:39:24 -0400

    **Updates** `purgebot` Now purges 100 bot messages if no amount is given. `crown` shows when the crown exchange will occur **New Commands** `members` displays members with the given command.

--- src/commands/mod/members.js
+++ src/commands/mod/members.js
@@ -19,7 +19,17 @@ module.exports = class MembersCommand extends Command {
     if (args[0].startsWith("<@&") || (/^[0-9]{18}$/g).test(args[0])) role = this.getRoleFromMention(message, args[0]) || message.guild.roles.cache.get(args[0]);
     else role = message.guild.roles.cache.find(r=> r.name.toLowerCase().startsWith(args[0].toLowerCase()))
 
+
     if (!role) return this.sendErrorMessage(message, 0, `Failed to find that role, try using a role ID`);
+    let description = ``;
+    for (const m of role.members)
+    {
+      description += `${m}`
+    }
+    const embed = new MessageEmbed()
+        .setTitle(`Members of ${role.name}`)
+        .setDescription(description)
+    message.channel.send(embed)
     console.log(role.members)
   }
 };
