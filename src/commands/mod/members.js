44b33a70d9cc45e385b41e39f5dfe2b0c1c23298
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-25 15:56:54 -0400

    **Updates** `purgebot` Now purges 100 bot messages if no amount is given. `crown` shows when the crown exchange will occur **New Commands** `members` displays members with the given command.

--- src/commands/mod/members.js
+++ src/commands/mod/members.js
@@ -22,7 +22,9 @@ module.exports = class MembersCommand extends Command {
 
     if (!role) return this.sendErrorMessage(message, 0, `Failed to find that role, try using a role ID`);
     let description;
-    console.log(role.members)
+    role.members.forEach(m => {
+      console.log(m.user)
+    })
     // for (const m of role.members)
     // {
     //   description += `<@${m.user.id}> `
