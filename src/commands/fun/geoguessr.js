eae19763378b598a1d912bc6d6b8290f9f8c814a
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-27 19:39:30 -0400

    Added SoP base

--- src/commands/fun/geoguessr.js
+++ src/commands/fun/geoguessr.js
@@ -7,19 +7,70 @@ const { oneLine } = require('common-tags');
 module.exports = class geoGuessrCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'smashorpass',
-      aliases: ['sop', 'match', 'smash'],
-      usage: 'smashorpass',
+      name: 'geoguessr',
+      aliases: ['geo', 'gg', 'geoguesser'],
+      usage: 'geoguessr',
       description: oneLine`
-        Play a game of smash or pass, you are shown a random user, you decide to smash or pass.
-        Cost: 25 points per like
+        Compete against your friends in a game of geoGuessr (anyone can answer).
+        Correct answer rewards 2 points.
+        The question will expire after 15 seconds.
       `,
       type: client.types.FUN,
-      examples: ['match', 'smash', 'smashorpass']
+      examples: ['geoguessr']
     });
   }
   run(message, args) {
     const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id);
-    message.reply('This feature is in development at the moment!')
+    const topic = message.client.topics[Math.floor(Math.random() * message.client.topics.length)];
+    // Get question and answers
+    const path = __basedir + '/data/geoguessr/' + topic + '.yml';
+    const questions = YAML.parse(fs.readFileSync(path, 'utf-8')).questions;
+    const n = Math.floor(Math.random() * questions.length);
+    const question = questions[n].question;
+    const answers = questions[n].answers;
+    const origAnswers = [...answers].map(a => `\`${a}\``);
+    // Clean answers
+    for (let i = 0; i < answers.length; i++) {
+      answers[i] = answers[i].trim().toLowerCase().replace(/\.|'|-|\s/g, '');
+    }
+
+    // Get user answer
+    const questionEmbed = new MessageEmbed()
+      .setTitle('geoGuessr')
+      .addField('Topic', `\`${topic}\``)
+      .addField('Question', `${question}`)
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    const url = question.match(/\bhttps?:\/\/\S+/gi);
+    if (url) questionEmbed.setImage(url[0]);
+    message.channel.send(questionEmbed);
+    let winner;
+    const collector = new MessageCollector(message.channel, msg => {
+      if (!msg.author.bot) return true;
+    }, { time: 15000 }); // Wait 15 seconds
+    collector.on('collect', msg => {
+      if (answers.includes(msg.content.trim().toLowerCase().replace(/\.|'|-|\s/g, ''))) {
+        winner = msg.author;
+        collector.stop();
+      }
+    });
+    collector.on('end', () => {
+      const answerEmbed = new MessageEmbed()
+        .setTitle('geoGuessr')
+        .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor(message.guild.me.displayHexColor);
+      if (winner)
+      {
+        message.client.db.users.updatePoints.run({ points: 2 }, winner.id, message.guild.id);
+        message.channel.send(answerEmbed.setDescription(`Congratulations ${winner}, you gave the correct answer! +2 Points!`));
+      }
+
+      else message.channel.send(answerEmbed
+        .setDescription('Sorry, time\'s up! Better luck next time.')
+        .addField('Correct Answers', origAnswers.join('\n'))
+      );
+    });
   }
 };
---
+++ src/commands/fun/smashorpass.js
@@ -0,0 +1,27 @@
+const Command = require('../Command.js');
+const { MessageEmbed, MessageCollector } = require('discord.js');
+const ReactionMenu = require('../ReactionMenu.js');
+const { oneLine } = require('common-tags');
+
+module.exports = class geoGuessrCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'smashorpass',
+      aliases: ['sop', 'smash'],
+      usage: 'smashorpass',
+      description: oneLine`
+        Play a game of smash or pass. You will be shown a random user and you vote smash or pass.
+        If there's a match, your discord username is revealed to them.
+        
+        Cost: 25 points per smash
+      `,
+      type: client.types.FUN,
+      examples: ['smashorpass', 'sop', 'smash']
+    });
+  }
+  run(message, args) {
+    const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id);
+    const potentialMatchRow = message.client.db.matches.getPotentialMatch.get(message.author.id)
+    console.log(potentialMatchRow)
+  }
+};
