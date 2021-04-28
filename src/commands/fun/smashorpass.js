beeb1a028d8974b83745a5c24ed988fd52cd7fd0
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-27 21:16:08 -0400

    Added SoP base

--- src/commands/fun/smashorpass.js
+++ src/commands/fun/smashorpass.js
@@ -24,26 +24,26 @@ module.exports = class geoGuessrCommand extends Command {
     let points = message.client.db.users.selectPoints.get(message.author.id, message.guild.id)
     if (points < 10) return message.reply(`**You need ${cost-points} more points in this server to play Smash or Pass .**\n\nTo check your points, type \`${prefix}points\``)
 
-    while (points > 10)
-    {
-      const potentialMatchRow = message.client.db.matches.getPotentialMatch.get(message.author.id)
-      console.log(potentialMatchRow.bio)
+    const potentialMatchRow = message.client.db.matches.getPotentialMatch.get(message.author.id)
 
-      const guild = message.client.guilds.cache.get(potentialMatchRow.guild_id)
-      const potentialMatchUser = guild.members.cache.get(potentialMatchRow.user_id)
+    const guild = message.client.guilds.cache.get(potentialMatchRow.guild_id)
+    const potentialMatchUser = guild.members.cache.get(potentialMatchRow.user_id)
 
-      let bio = `*${potentialMatchUser.displayName} has not set a bio yet.*`
-      if (potentialMatchRow.bio != null) bio = `${potentialMatchUser.displayName}'s Bio:\n${potentialMatchRow.bio}`
+    let bio = `*${potentialMatchUser.displayName} has not set a bio yet.*`
+    if (potentialMatchRow.bio != null) bio = `${potentialMatchUser.displayName}'s Bio:\n${potentialMatchRow.bio}`
 
-      const embed = new MessageEmbed()
-          .setTitle(`🔥 Smash or Pass 👎`)
-          .setDescription(bio)
-          .setImage(potentialMatchUser.user.displayAvatarURL({ dynamic: true, size: 512 }))
-          .setFooter(`Expires in 10 seconds.`)
+    const embed = new MessageEmbed()
+        .setTitle(`🔥 Smash or Pass 👎`)
+        .setDescription(bio)
+        .setImage(potentialMatchUser.user.displayAvatarURL({ dynamic: true, size: 512 }))
+        .setFooter(`Expires in 10 seconds.`)
 
-      const deletion = await message.channel.send(embed).then(async msg=> {
+    await message.channel.send(embed).then(async msg=> {
+      let stop = false;
+      while (points > 10 && stop == false)
+      {
         const d = new Date();
-        const reactions = await confirm(msg, message.author, ["🔥", "👎"], 10000);
+        const reactions = await confirm(msg, message.author, ["🔥", "⏹","👎"], 10000);
         if(reactions === "🔥") {
           message.client.db.users.updatePoints.run({ points: -cost }, message.author.id, message.guild.id);
           message.client.db.matches.insertRow.run(message.author.id, potentialMatchUser.id, 'yes', d.toISOString())
@@ -54,9 +54,10 @@ module.exports = class geoGuessrCommand extends Command {
           msg.edit(new MessageEmbed().setTitle(`👎 Passed ${potentialMatchUser.displayName}`).setDescription(`Loading...`))
         }
         else {
-          console.log("Timed Out")
+          stop = true;
+          msg.edit(`Stopped playing Smash or Pass!`)
         }
-      })
-    }
+      }
+    })
   }
 };
