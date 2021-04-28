fc094ef87c51062a9ccfd0dc33ff8faf0acef4a4
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-28 00:11:17 -0400

    Finished Smash or pass

--- src/commands/fun/smashorpass.js
+++ src/commands/fun/smashorpass.js
@@ -114,7 +114,7 @@ module.exports = class geoGuessrCommand extends Command {
     else
     {
       const member = await this.getMemberFromMention(message, args[0]) || await message.guild.members.cache.get(args[0] || await message.guild.members.cache.find(m=>m.displayName.toLowerCase().startsWith(args[0].toLowerCase())));
-
+      console.log(member)
       const row = message.client.db.matches.getSeenByUser.get(message.author.id, member.user.id)
       if (row != null || row !== undefined)
       {
