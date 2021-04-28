c61484a8eb0e2f5c42da2a8ea37a87a1af275ad9
rehman <hayaturehmanahmadzai@gmail.com>
2021-04-28 06:32:00 -0400

    fixing match

--- src/commands/fun/smashorpass.js
+++ src/commands/fun/smashorpass.js
@@ -82,7 +82,7 @@ module.exports = class smashOrPassCommand extends Command {
             message.client.db.users.updatePoints.run({ points: -cost }, message.author.id, message.guild.id);
             message.client.db.matches.insertRow.run(message.author.id, potentialMatchUser.user.id, 'yes', d.toISOString())
             points = points - cost
-            const matched = message.client.db.matches.getMatch.get(message.author.id, potentialMatchUser.user.id)
+            const matched = await message.client.db.matches.getMatch.get(message.author.id, potentialMatchUser.user.id)
             console.log(`matched`)
             console.log(matched)
             if (matched != null)
