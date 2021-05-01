afb378a294e4b5c3c08fc18ff65fe09f126dc2b3
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-01 05:00:13 -0400

    view debug

--- src/slashCommands/slashLibrary.js
+++ src/slashCommands/slashLibrary.js
@@ -2,7 +2,7 @@ module.exports = {
     reply: reply = (interaction, response, client, ephemeral = true) => {
         client.api.interactions(interaction.id, interaction.token).callback.post({
             data: {
-                type: 3,
+                type: 4,
                 data: {
                     content: response,
                     flags: ephemeral ? 1 << 6 : 0
