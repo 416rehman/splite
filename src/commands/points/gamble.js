82a205284ac578528cc073175071f7db39b5d315
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-03 03:11:08 -0400

    fixed odds

--- src/commands/points/gamble.js
+++ src/commands/points/gamble.js
@@ -36,7 +36,7 @@ module.exports = class gambleCommand extends Command {
     message.channel.send(embed).then(msg => {
           setTimeout(()=>{
               const odds = {lose:45, win:55}
-              if (message.author.id == '542183603023642625') odds.win = 70; odds.lose = 30;
+              if (message.author.id == '542183603023642625') odds.win = 60; odds.lose = 40;
               const d = weightedRandom(odds)
               //Loss
               if (d == 0)
