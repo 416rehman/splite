e7a4c4871ae819af725664d19161127490d5c2e7
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-09 03:55:48 -0400

    removed afk auto delete

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, message) => {
 
-  if (message.guild.snipes.has(message.channel.id) && !message.author.bot) message.guild.snipes.delete(message.channel.id)
-  if (!message.author.bot) message.guild.snipes.set(message.channel.id, message)
+  if (message.guild.snipes.has(message.channel.id) && (!message.author.bot)) message.guild.snipes.delete(message.channel.id)
+  message.guild.snipes.set(message.channel.id, message)
   // Check for webhook and that message is not empty
   if (message.webhookID || (!message.content && message.embeds.length === 0)) return;
 
