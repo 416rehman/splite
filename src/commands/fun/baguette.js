5678a14b3c3acff3920bd6444c4ff36156e9468e
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 04:32:33 -0400

    added fun commands

---
+++ src/commands/fun/baguette.js
@@ -0,0 +1,35 @@
+const Command = require('../Command.js');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const {fail, load} = require("../../utils/emojis.json")
+
+module.exports = class baguetteCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'baguette',
+      aliases: ['threat'],
+      usage: 'baguette <user mention/id>',
+      description: 'Generates a baguette image',
+      type: client.types.FUN,
+      examples: ['baguette @split']
+    });
+  }
+  async run(message, args) {
+    if (message.guild.funInProgress.has(message.author.id)) return message.channel.send(new MessageEmbed().setDescription(`${fail} Please wait, you already have a request pending.`))
+    message.guild.funInProgress.set(message.author.id, 'fun');
+    const member = await this.getMemberFromMention(message, args[0]) || await message.guild.members.cache.get(args[0]) || message.author;
+
+    message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
+      try {
+        const buffer = await msg.client.nekoApi.generate("baguette", { url: this.getAvatarURL(member) })
+        const attachment = new MessageAttachment(buffer, "baguette.png");
+
+        await message.channel.send(attachment)
+        await msg.delete()
+      }
+      catch (e) {
+        await msg.edit(new MessageEmbed().setDescription(`${fail} ${e}`))
+      }
+    })
+    message.guild.funInProgress.delete(message.author.id)
+  }
+};
