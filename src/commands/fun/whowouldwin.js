101fc9fc2bd1f9d209d2a0e6a27533fd22ef70bc
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 05:03:06 -0400

    added fun commands

--- src/commands/fun/whowouldwin.js
+++ src/commands/fun/whowouldwin.js
@@ -1,7 +1,7 @@
 const Command = require('../Command.js');
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const {fail, load} = require("../../utils/emojis.json")
-
+const fetch = require('node-fetch')
 module.exports = class whowouldwinCommand extends Command {
   constructor(client) {
     super(client, {
@@ -18,16 +18,12 @@ module.exports = class whowouldwinCommand extends Command {
     message.guild.funInProgress.set(message.author.id, 'fun');
     const member = await this.getMemberFromMention(message, args[0]) || await message.guild.members.cache.get(args[0]) || message.author;
     const member2 = await this.getMemberFromMention(message, args[1]) || await message.guild.members.cache.get(args[1]) || message.author;
-    console.log(this.getAvatarURL(member))
-    console.log(this.getAvatarURL(member2))
+
     message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
       try {
-        const buffer = await msg.client.ameApi.generate("whowouldwin", {
-          user1: this.getAvatarURL(member),
-          user2: this.getAvatarURL(member2),
-          raw: 1
-        });
-        const attachment = new MessageAttachment(buffer, "whowouldwin.png");
+        const res = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=whowouldwin&user1=${this.getAvatarURL(member)}&user2=${this.getAvatarURL(member2)}`));
+        const json = await res.json();
+        const attachment = new MessageAttachment(json.message, "whowouldwin.png");
 
         await message.channel.send(attachment)
         await msg.delete()
