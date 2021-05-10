a8d931abc697d411e12c27ea3eec504c9aab081e
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 06:24:28 -0400

    added fun commands

--- src/commands/fun/nsfw.js
+++ src/commands/fun/nsfw.js
@@ -6,31 +6,31 @@ module.exports = class thighsCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'nsfw',
-      aliases: ['nsfw'],
+      aliases: ['18+'],
       usage: 'thighs <user mention/id>',
-      description: 'Random nsfw image',
+      description: 'Random nsfw image/gif',
       type: client.types.FUN,
       nsfwOnly: true,
-      examples: ['nsfw']
+      examples: ['nsfw boobs', 'nsfw thigh']
     });
   }
   async run(message, args) {
     message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
       try {
-        var types = Array('hass', 'hmidriff', 'pgif', '4k', 'hentai', 'holo', 'hneko', 'neko', 'hkitsune', 'kemonomimi', 'anal', 'hanal', 'gonewild', 'kanna', 'ass', 'pussy', 'thigh', 'hthigh', 'gah','paizuri', 'tentacle', 'boobs', 'hboobs', 'yaoi');
+        var types = Array('hass', 'pgif', '4k', 'hentai', 'hneko', 'hkitsune', 'kemonomimi', 'anal', 'hanal', 'gonewild', 'ass', 'pussy', 'thigh', 'hthigh','paizuri', 'tentacle', 'boobs', 'hboobs');
         let chosen
         if (args[0])
         {
           if (types.includes(args[0])) chosen = types.find(e => e === args[0])
           else {
             const description = types.join('\n')
-            return msg.edit(new MessageEmbed().setDescription(`${fail} Image Type **${args[0]}** Invalid!\nSupported Image Types:\n${description}`))
+            return msg.edit(new MessageEmbed().setDescription(`${fail} Category **${args[0]}** Invalid!\nSupported Image Types:\n${description}`))
           }
         }
         else chosen = types[Math.floor(Math.random() * types.length)];
-
+        const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id); // Get prefix
         const buffer = await msg.client.nekoApi.get(chosen)
-        await msg.edit(new MessageEmbed().setDescription(`Image Type: ${chosen}`).setImage(buffer))
+        await msg.edit(new MessageEmbed().setDescription(`Category: **${chosen}**`).setImage(buffer).setFooter(`Specify category like this, ${prefix}nsfw boobs`))
       }
       catch (e) {
         await msg.edit(new MessageEmbed().setDescription(`${fail} ${e}`))
