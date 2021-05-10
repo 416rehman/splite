25da72977159f8292f0c1dc2c9a46dd4434f86cc
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 02:52:08 -0400

    added fun commands

--- src/commands/fun/brazzers.js
+++ src/commands/fun/brazzers.js
@@ -15,9 +15,7 @@ module.exports = class BrazzersCommand extends Command {
   }
   async run(message, args) {
     console.log(args)
-    let member = message.author;
-
-    if (args[0]) member = await this.getMemberFromMention(message, args[0]) || await message.guild.members.cache.get(args[0]);
+    const member = await this.getMemberFromMention(message, args[0]) || await message.guild.members.cache.get(args[0]) || message.author;
     console.log(member)
 
     message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
