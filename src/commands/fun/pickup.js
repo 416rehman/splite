065d554d652f7dce181b2594b868c7b9bfc0482a
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 08:23:57 -0400

    added fun commands

---
+++ src/commands/fun/pickup.js
@@ -0,0 +1,33 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = class pickupCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'pickup',
+      usage: 'pickup',
+      description: 'Create a pickup line and send it to someone',
+      type: client.types.FUN,
+      examples: ['pickup @split']
+    });
+  }
+  async run(message, args) {
+    const member = await this.getMemberFromMention(message, args[0]) || await message.guild.members.cache.get(args[0]) || message.author;
+    try {
+      const res = await fetch('http://www.pickuplinegen.com/');
+      const pickup = (await res.text());
+      console.log(pickup)
+
+      const embed = new MessageEmbed()
+        .setDescription(`<@${member.id}>, ${pickup}`)
+        .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor(message.guild.me.displayHexColor);
+      message.channel.send(embed);
+    } catch (err) {
+      message.client.logger.error(err.stack);
+      this.sendErrorMessage(message, 1, 'Please try again in a few seconds', err.message);
+    }
+  }
+};
