20748ea6adb6b2a192b8d44fb60b9217d71b9d1d
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 02:45:14 -0400

    added fun commands

--- src/commands/fun/brazzers.js
+++ src/commands/fun/brazzers.js
@@ -16,13 +16,14 @@ module.exports = class BrazzersCommand extends Command {
   async run(message, args) {
     // Get message
     let member;
+
     if (!args[0]) member = message.author
     else member = this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
     if (!member) return this.sendErrorMessage(message, 0, `Please mention a user or provide a valid user ID`);
 
     message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
       try {
-        const buffer = await msg.client.ameApi.generate("brazzers", { url: message.author.displayAvatarURL({ format: "png", size: 512 }) });
+        const buffer = await msg.client.ameApi.generate("brazzers", { url: member.displayAvatarURL({ format: "png", size: 512 }) });
         const attachment = new MessageAttachment(buffer, "brazzers.png");
 
         await message.channel.send(attachment)
