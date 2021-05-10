a84ba40f3868e4d8376f2c2d1cfdeb59bfc9d399
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 06:04:32 -0400

    added fun commands

--- src/commands/fun/nsfw.js
+++ src/commands/fun/nsfw.js
@@ -2,21 +2,22 @@ const Command = require('../Command.js');
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const {fail, load} = require("../../utils/emojis.json")
 
-module.exports = class hassCommand extends Command {
+module.exports = class thighsCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'hass',
-      aliases: [],
-      usage: 'hass <user mention/id>',
-      description: 'Random hentai/ass image',
-      type: client.types.NSFW,
-      examples: ['hass']
+      name: 'thighs',
+      aliases: ['thigh'],
+      usage: 'thighs <user mention/id>',
+      description: 'Random image of thighs',
+      type: client.types.FUN,
+      nsfwOnly: true,
+      examples: ['thighs']
     });
   }
   async run(message, args) {
     message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
       try {
-        const buffer = await msg.client.nekoApi.get("hass")
+        const buffer = await msg.client.nekoApi.get("thigh")
         await msg.edit(new MessageEmbed().setDescription(`\u200b`).setImage(buffer))
       }
       catch (e) {
