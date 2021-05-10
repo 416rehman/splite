c759c70d9b0c7e92a1538559187dcf2260826bf6
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 02:47:31 -0400

    added fun commands

--- src/commands/fun/brazzers.js
+++ src/commands/fun/brazzers.js
@@ -7,7 +7,7 @@ module.exports = class BrazzersCommand extends Command {
     super(client, {
       name: 'brazzers',
       aliases: [],
-      usage: '`brazzers <user mention/id>`',
+      usage: 'brazzers <user mention/id>',
       description: 'Generates a brazzers image',
       type: client.types.FUN,
       examples: ['brazzers @split']
@@ -19,11 +19,10 @@ module.exports = class BrazzersCommand extends Command {
 
     if (!args[0]) member = message.author
     else member = this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
-    if (!member) return this.sendErrorMessage(message, 0, `Please mention a user or provide a valid user ID`);
 
     message.channel.send(new MessageEmbed().setDescription(`${load} Loading...`)).then(async msg=>{
       try {
-        const buffer = await msg.client.ameApi.generate("brazzers", { url: member.displayAvatarURL({ format: "png", size: 512 }) });
+        const buffer = await msg.client.ameApi.generate("brazzers", { url: member.avatarURL({ format: "png", size: 512 }) });
         const attachment = new MessageAttachment(buffer, "brazzers.png");
 
         await message.channel.send(attachment)
