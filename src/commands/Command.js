075d4e1dc060f9c698fb6fc0de60a45bb953f6e4
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-10 05:50:55 -0400

    added fun commands

--- src/commands/Command.js
+++ src/commands/Command.js
@@ -81,7 +81,7 @@ class Command {
      * If command can only be used by owner
      * @type {boolean}
      */
-    console.log(this.type)
+    console.log(typeof this.type)
     this.nsfwOnly = options.nsfwOnly || false;
 
     /**
