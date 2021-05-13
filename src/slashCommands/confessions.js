3dc15d73299b8a333998e019393200e2f5cccc4b
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-13 01:17:44 -0400

    alternating confessions footer

--- src/slashCommands/confessions.js
+++ src/slashCommands/confessions.js
@@ -37,7 +37,7 @@ module.exports = {
             n = (n.toString())
             n = n.slice(n.length - 6)
             const alternate = client.utils.weightedRandom({0: 50, 1: 50})
-            const ftr = alternate == 0 ? `Report ToS-breaking or hateful confessions by using /report [confessionID]` : `Type "/confess" in any channel to post a confession here. `
+            const ftr = alternate == 0 ? `Report ToS-breaking or hateful confessions by using /report [confessionID]` : `Type "/confess" in any channel to post a confession here.`
             const embed = new MessageEmbed()
                 .setTitle(`Confession ID: ${n}`)
                 .setDescription(`"${confession}"`)
