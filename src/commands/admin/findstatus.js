cde1bb66e6cc86eb85995535ae8748b83e4cef04
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-16 00:04:36 -0400

    added perms to whois

--- src/commands/admin/findstatus.js
+++ src/commands/admin/findstatus.js
@@ -28,7 +28,7 @@ module.exports = class findStatusCommand extends Command {
     if(args.length <= 0) return message.reply('Please provide text to search for.')
 
     const query = (message.content.slice(message.content.indexOf(args[0]), message.content.length)).toLowerCase();
-    if (query.length > 50 || query.length < 5) return message.reply('Please provide a text with size of 5 to 50 characters')
+    if (query.length > 50 || query.length < 3) return message.reply('Please provide a text with length of 3 to 50 characters')
 
     const embed = new MessageEmbed()
         .setDescription(`${emojis.load} **Searching for users with status **\n\`\`\`${query}\`\`\``)
