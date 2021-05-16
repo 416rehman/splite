b3a0ed66d6adae8dc0e01f96d0bdd8e71c3ae6ba
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-16 10:13:19 -0400

    added owner commands

--- src/commands/owner/history.js
+++ src/commands/owner/history.js
@@ -14,23 +14,42 @@ module.exports = class ServersCommand extends Command {
     });
   }
   async run(message, args) {
-    message.client.channels.cache.get(args[0]).messages.fetch({ limit: 100 })
-      .then(async msgs => {
-          const history = msgs.filter(m=>!m.author.bot).array().map(msg=>{
-            return `${msg.author.tag}\n${msg.content.length > 0 ? `\`\`\`${msg.content}\`\`\`` : ''}${ msg.attachments ? msg.attachments.array().map(att=>{return att.url}).join('\n'):'no attachments'}\n--------------------------------`
-          })
-        const embed = new MessageEmbed()
-            .setTitle('History of ' + message.client.channels.cache.get(args[0]).name)
-            .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
-            .setColor(message.guild.me.displayHexColor);
+    const target = message.client.channels.cache.get(args[0]) || message.client.guilds.cache.get(args[0]) || null;
 
-        if (history.length <= 10) {
-          const range = (history.length == 1) ? '[1]' : `[1 - ${history.length}]`;
-          await message.channel.send(embed.setTitle(`History ${range}`).setDescription(history.join('\n')));
-        } else {
-          new ReactionMenu(message.client, message.channel, message.member, embed, history);
+    if (!target) await message.reply(`Failed to find a channel/server with provided ID`);
+    let history = [];
+    if (target.constructor.name === 'Channel') history = this.getMessagesFromChannel(target);
+    else if (target.constructor.name === 'Guild')
+    {
+        for (ch of target.channels.cache)
+        {
+            const temp = await this.getMessagesFromChannel(ch)
+            history.concat(temp)
         }
-      })
+    }
+    const embed = new MessageEmbed()
+      .setTitle('History of ' + message.client.channels.cache.get(args[0]).name)
+      .setFooter(message.member.displayName, message.author.displayAvatarURL({dynamic: true}))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+
+    if (history.length <= 10) {
+      const range = (history.length == 1) ? '[1]' : `[1 - ${history.length}]`;
+      await message.channel.send(embed.setTitle(`History ${range}`).setDescription(history.join('\n')));
+    } else {
+      new ReactionMenu(message.client, message.channel, message.member, embed, history);
+    }
   }
+
+    getMessagesFromChannel(channel) {
+        channel.messages.fetch({limit: 100})
+            .then(async msgs => {
+                const history = msgs.filter(m => !m.author.bot).array().map(msg => {
+                    return `${msg.author.tag}\n${msg.content.length > 0 ? `\`\`\`${msg.content}\`\`\`` : ''}${msg.attachments ? msg.attachments.array().map(att => {
+                        return att.url
+                    }).join('\n') : 'no attachments'}\n--------------------------------`
+                })
+                return history;
+            })
+    }
 };
