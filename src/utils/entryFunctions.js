c36c144691e9800622c13993450a83502b29d8b3
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-16 01:23:59 -0400

    added perms to whois

--- src/utils/entryFunctions.js
+++ src/utils/entryFunctions.js
@@ -3,16 +3,12 @@ const {MessageEmbed} = require('discord.js');
 module.exports = {
     joinvoting: async function joinvoting(reaction, user, client, timer, duration, messageID, votingChannelID, emoji) {
         if (reaction.message.id === messageID) {
-            let proceed = false;
-            if (reaction.emoji.id) if(reaction.emoji.id === emoji) proceed = true;
-            else if(reaction.emoji.name === emoji) proceed = true;
-            console.log('before proceed')
-            if (proceed) {
-                console.log('proceeding')
+            if (reaction.emoji.id && reaction.emoji.id === emoji || reaction.emoji.name === emoji) {
                 if (reaction.message.guild.JoinVotingInProgress.has(user.id)) return;
-                console.log('Good')
+                reaction.message.guild.gamblesInProgress.set(user.id, new Date().getTime().toString())
                 let time = duration*1000;
                 await user.send("Please wait, server members are deciding your fate.").catch(() => console.log("Can't send DM to your user!"));
+
                 let embed = new MessageEmbed()
                     .setThumbnail(`${user.displayAvatarURL({ dynamic: true })}`)
                     .setColor(0xf25852)
@@ -22,10 +18,9 @@ module.exports = {
                     .setFooter(`Voting will end in ${duration} seconds...`);
                 const channel = client.channels.cache.find(channel => channel.id === votingChannelID)
                 channel.send(embed).then(msg => {
-                    reaction.message.guild.gamblesInProgress.set(user.id, new Date().getTime().toString())
                     msg.react('👍').then(() => msg.react('👎'));
 
-                    embed = new MessageEmbed()
+                    embed
                         .setThumbnail(`${user.displayAvatarURL({ dynamic: true })}`)
                         .setColor(0xf25852)
                         .setTitle(`${user.username}#${user.discriminator} Is Attempting To Join Server`)
@@ -34,11 +29,12 @@ module.exports = {
                         .setFooter(`Voting will end in ${duration = duration - timer} seconds...`);
 
                     let myinterval = setInterval(() => {
-                        msg.edit(embed).then(() => embed = new MessageEmbed()
-                            .setThumbnail(`${user.displayAvatarURL({ dynamic: true })}`)
-                            .setColor(0xf25852)
-                            .setTitle(`${user.username}#${user.discriminator} Is Attempting To Join Server`)
-                            .setDescription(`Vote and decide their fate.\n
+                        msg.edit(embed).then(() =>
+                            embed
+                                .setThumbnail(`${user.displayAvatarURL({ dynamic: true })}`)
+                                .setColor(0xf25852)
+                                .setTitle(`${user.username}#${user.discriminator} Is Attempting To Join Server`)
+                                .setDescription(`Vote and decide their fate.\n
                 **Should they stay?**\n`)
                             .setFooter(`Voting will end in ${duration = duration - timer} seconds...`));
                     }, timer*1000);
@@ -51,7 +47,6 @@ module.exports = {
                             const reaction = collected.first();
                         })
                         .catch(async collected => {
-                            reaction.message.guild.gamblesInProgress.delete(message.author.id)
                             clearInterval(myinterval)
                             let yesVotes = (msg.reactions.cache.get('👍').count)
                             let noVotes = (msg.reactions.cache.get('👎').count)
@@ -59,31 +54,34 @@ module.exports = {
                                 await user.send(`Sorry, you were voted off of the server. You received ${yesVotes} 👍 and ${noVotes} 👎`).catch(() => console.log("Can't send DM to your user!"));
                                 try {await msg.guild.members.cache.get(user.id).ban({days: 0, reason: 'Splite JoinVoting - Voted Off'})}
                                 catch{
-                                    embed = new MessageEmbed()
+                                    embed
                                         .setColor("RED")
                                         .setTitle(`Failed to ban ${user.username}#${user.discriminator}!`)
                                         .setImage("https://media4.giphy.com/media/ljtfkyTD3PIUZaKWRi/giphy.gif")
-                                        .setDescription(`**Maybe they have higher perms than me?**\nThey received ${yesVotes} 👍 and ${noVotes} 👎`);
+                                        .setDescription(`**Maybe they have higher perms than me?**\nThey received ${yesVotes} 👍 and ${noVotes} 👎`)
+                                        .setFooter('');
                                     msg.edit(embed)
-                                    return;
                                 }
 
-                                embed = new MessageEmbed()
+                                embed
                                     .setColor("RED")
                                     .setTitle(`${user.username}#${user.discriminator} was voted off the server!`)
                                     .setImage("https://media.tenor.com/images/da66a96ca7f65f949a07db8ab9926297/tenor.gif")
-                                    .setDescription(`They received ${yesVotes} 👍 and ${noVotes} 👎`);
+                                    .setDescription(`They received ${yesVotes} 👍 and ${noVotes} 👎`)
+                                    .setFooter('');
                                 msg.edit(embed)
 
                             } else {
                                 await user.send('Welcome, enjoy your stay!.').catch(() => console.log("Can't send DM to your user!"));
-                                embed = new MessageEmbed()
+                                embed
                                     .setColor("GREEN")
                                     .setImage("https://i.pinimg.com/originals/5e/78/af/5e78affab2547d678e4c5458dd931381.gif")
                                     .setTitle(`${user.username}#${user.discriminator} has been welcomed to the server!`)
-                                    .setDescription(`They received ${yesVotes} 👍 and ${noVotes} 👎`);
+                                    .setDescription(`They received ${yesVotes} 👍 and ${noVotes} 👎`)
+                                    .setFooter(' ')
                                 msg.edit(embed)
                             }
+                            reaction.message.guild.gamblesInProgress.delete(message.author.id)
                         });
                 })
             }
