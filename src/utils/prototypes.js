4ca478c05db0e816386574e15cea8d993aaffbca
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-15 22:51:00 -0400

    added perms to whois

--- src/utils/prototypes.js
+++ src/utils/prototypes.js
@@ -1,4 +1,4 @@
-exports.arrayProto = function(constructor) {
+module.exports.arrayProto = function(constructor) {
     constructor.prototype.move = function(from, to) {
         this.splice(to, 0, this.splice(from, 1)[0]);
     };
