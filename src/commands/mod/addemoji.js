f3d58ce3fa5f056044d8dbe5ad844720ebd60403
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-18 15:37:30 -0400

    added addemoji

--- src/commands/mod/addemoji.js
+++ src/commands/mod/addemoji.js
@@ -20,36 +20,48 @@ module.exports = class AddRoleCommand extends Command {
       let name;
       const urlRegex = new RegExp(/^(ftp|http|https):\/\/[^ "]+$/)
 
-      const emoji = args[0];
-      if (!emoji) this.sendErrorMessage(message, 0, 'Please mention a valid emoji.');
-
-      let customemoji = Discord.Util.parseEmoji(emoji) //Check if it's a emoji
-
-      if (customemoji.id) {
-        const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
-            customemoji.animated ? "gif" : "png"
-        }`
-        name = args.slice(1).join("_")
-        const emoji = await message.guild.emojis.create(
-            `${Link}`,
-            `${name || `${customemoji.name}`}`
-        );
-        return message.channel.send(`${emoji} added with name "${customemoji.name}"`);
-      } else if (urlRegex.test(args[0])) { //check for image urls
-        name = args.slice(1).join("_") || Math.random().toString(36).slice(2) //make the name compatible or just choose a random string
-        const addedEmoji = await message.guild.emojis.create(
-            `${emoji}`,
-            `${name || `${customemoji.name}`}`
-        );
-        return message.channel.send(`${addedEmoji} added with name "${addedEmoji.name}"`);
-      } else {
-        let CheckEmoji = parse(emoji, { assetType: "png" });
-        if (!CheckEmoji[0])
-          return this.sendErrorMessage(message, 0, 'Please mention a valid emoji.');
+      if (args.length > 2)
+      {
+        args.forEach(emoji => {
+          addEmoji(emoji)
+        })
       }
+      else addEmoji(args[0], args.slice(1).join("_"))
+
     } catch (err) {
       this.client.logger.error(err)
       this.sendErrorMessage(message, 1, 'A error occured while adding the emoji. Common reasons are:- unallowed characters in emoji name, 50 emoji limit.', err)
     }
   }
+}
+
+async function addEmoji(emoji, emojiName)
+{
+  if (!emoji) this.sendErrorMessage(message, 0, 'Please mention a valid emoji.');
+  let name
+  let customemoji = Discord.Util.parseEmoji(emoji) //Check if it's a emoji
+
+  if (customemoji.id) {
+    const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
+        customemoji.animated ? "gif" : "png"
+    }`
+    name = emojiName
+    const emoji = await message.guild.emojis.create(
+        `${Link}`,
+        `${name || `${customemoji.name}`}`
+    );
+    return message.channel.send(`${emoji} added with name "${customemoji.name}"`);
+  } else if (urlRegex.test(emoji)) { //check for image urls
+    name = emojiName || Math.random().toString(36).slice(2) //make the name compatible or just choose a random string
+    const addedEmoji = await message.guild.emojis.create(
+        `${emoji}`,
+        `${name || `${customemoji.name}`}`
+    );
+    return message.channel.send(`${addedEmoji} added with name "${addedEmoji.name}"`);
+  }
+  else {
+      let CheckEmoji = parse(emoji, { assetType: "png" });
+      if (!CheckEmoji[0])
+        return this.sendErrorMessage(message, 0, 'Please mention a valid emoji.');
+    }
 }
