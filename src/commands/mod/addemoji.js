e495be06e2fb078798d16945a25777680fee4695
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-18 16:23:46 -0400

    added addemoji

--- src/commands/mod/addemoji.js
+++ src/commands/mod/addemoji.js
@@ -8,7 +8,7 @@ module.exports = class AddRoleCommand extends Command {
       name: 'addemoji',
       aliases: ['em', 'emoji', 'emojiadd'],
       usage: 'addemoji <emoji> <name>',
-      description: 'Add any of your preferred emoji from any server, or an image link.\nMultiple emojis can be added by typing all of them at once (Emoji name cannot be set if adding multiple).',
+      description: 'Add emoji from a server, or an image link.\nMultiple emojis can be added by typing all of them at once.',
       type: client.types.MOD,
       clientPermissions: ['SEND_MESSAGES', 'EMBED_LINKS', 'MANAGE_EMOJIS'],
       userPermissions: ['MANAGE_ROLES'],
@@ -17,21 +17,13 @@ module.exports = class AddRoleCommand extends Command {
   }
   async run(message, args){
     try {
-      console.log(args)
-      args = args.join(' ')
-      console.log(args)
-      args = args.replace(/</g , ' <')
-      args = args.trim()
-      console.log(args)
-      args = args.split(/\s+/)
-      console.log(args)
-      // if (args.length > 2)
-      // {
-      //   args.forEach(emoji => {
-      //     addEmoji(emoji, message, this)
-      //   })
-      // }
-      // else addEmoji(args[0], message, this, args.slice(1).join("_"))
+      if (args.length > 2)
+      {
+        args.forEach(emoji => {
+          addEmoji(emoji, message, this)
+        })
+      }
+      else addEmoji(args[0], message, this, args.slice(1).join("_"))
 
     } catch (err) {
       this.client.logger.error(err)
