fed0b26ffa31323ed6f73f4910c51834535fbb8c
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-19 19:16:33 -0400

    advanced role finding

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -77,10 +77,7 @@ module.exports = class HelpCommand extends Command {
           if (command.userPermissions && command.userPermissions.every(p => message.member.hasPermission(p)) && !all)
             commands[command.type].push(`\`${command.name}\``);
           else if (!command.userPermissions || all) {
-            if (!(command.name.includes('clear')))
-            {
               commands[command.type].push(`\`${command.name}\``);
-            }
           }
         }
       });
