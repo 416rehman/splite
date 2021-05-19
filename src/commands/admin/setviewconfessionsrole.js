9d7a8cee79abfcbf09cc151487bec6534abfdcb3
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-19 19:42:12 -0400

    advanced role finding

--- src/commands/admin/setviewconfessionsrole.js
+++ src/commands/admin/setviewconfessionsrole.js
@@ -7,7 +7,7 @@ module.exports = class SetViewConfessionsRoleCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'setviewconfessionsrole',
-      aliases: ['setvcr', 'svcr'],
+      aliases: ['setvcr', 'svcr', 'setviewconfessions', 'setviewconfession'],
       usage: 'setviewconfessionsrole <role mention/ID>',
       description: oneLine`
         Sets the role whose members can use /view command to view details about a confession.
