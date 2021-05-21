9d7d9c398195e709e49ec74226d1d7996eb803da
rehman <hayaturehmanahmadzai@gmail.com>
2021-05-20 23:51:24 -0400

    added imgflip integration

--- src/commands/fun/hate.js
+++ src/commands/fun/hate.js
@@ -22,7 +22,7 @@ module.exports = class HateCommand extends Command {
       try {
         const text = message.mentions.users.size > 0 ? message.mentions.users.first().username : args.join(' ')
         const buffer = await msg.client.utils.generateImgFlipImage(242461078, `${text}`, `${text}`)
-        console.log(buffer)
+
         if (buffer)
         {
           const attachment = new MessageAttachment(buffer, "allmyhomieshate.png");
