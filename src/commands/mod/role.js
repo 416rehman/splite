f68cea56112674e240ce4b237ebb6951819c3921
Rehman <certifiedrehman@gmail.com>
2021-08-30 02:41:41 -0400

    Added banner.js

--- src/commands/mod/role.js
+++ src/commands/mod/role.js
@@ -88,7 +88,7 @@ module.exports = class RoleCommand extends Command {
           .setTimestamp()
           .setColor(message.guild.me.displayHexColor);
     }
-    if (target.size) {
+    if (memberArg == 'humans' || memberArg == 'bots') {
       setTimeout(function () {
         return message.channel.send(embed);
       }, target.size*1.5*1000)
