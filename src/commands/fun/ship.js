b216bd76d87a4887ccfc185ac4ae9b190d1e771e
Rehman <certifiedrehman@gmail.com>
2021-09-05 08:05:37 -0400

    Ships will reset after 5 mins

--- src/commands/fun/ship.js
+++ src/commands/fun/ship.js
@@ -58,7 +58,7 @@ module.exports = class shipCommand extends Command {
   }
 
   addToCollection(message, owner, child, shipScore) {
-    if (message.guild.ships.has(owner.id) == false)  message.guild.ships.set(owner.id, [{userId: child.id, shipScore}])
+    if (message.guild.ships.has(owner.id) == false || new Date() - message.guild.ships.get(owner.id).time > 300000)  message.guild.ships.set(owner.id, [{userId: child.id, shipScore, time: Date.now()}])
     else {
       let matchedBefore
       let ships = message.guild.ships.get(owner.id)
