94f60f6926dcd7acf7dc5a9a470e935cff463902
Rehman <certifiedrehman@gmail.com>
2022-04-27 15:06:01 -0400

    added webhook handler

---
+++ src/webhooks/topgg.webhook.js
@@ -0,0 +1,22 @@
+const Webhook = require("./Webhook");
+module.exports = class TopGGWebhook extends Webhook {
+    constructor(client) {
+        super(client,{
+            name: 'topgg',
+            description: 'Receives TopGG vote events.',
+            authorization: client.config.apiKeys.topGG.webhook_mode.authorization,
+            requestsPerMinute: 60,
+            cooldownTime: 1000,
+            disabled: client.config.apiKeys.topGG.useMode !== 'webhook_mode'
+        });
+    }
+
+    async execute(body) {
+        return {
+            status: 200,
+            body: {
+                message: 'Received TopGG vote event.'
+            }
+        };
+    }
+}
