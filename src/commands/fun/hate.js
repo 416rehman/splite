f1b57055f08286a3bf2d215a451b337a79a22191
shawn yu <shawn.yu5@hotmail.com>
2022-04-26 22:28:32 -0400

    refactor complete

--- src/commands/fun/hate.js
+++ src/commands/fun/hate.js
@@ -1,37 +1,96 @@
-const Command = require('../Command.js');
-const {MessageEmbed, MessageAttachment} = require('discord.js');
-const {fail, load} = require("../../utils/emojis.json")
+const Command = require("../Command.js");
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const { fail, load } = require("../../utils/emojis.json");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = class HateCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'hate',
-            aliases: ['fuck', 'allmyhomies', 'homies'],
-            usage: 'hate <text>',
-            description: 'Generates an "all my homies hate" image with provided text',
-            type: client.types.FUN,
-            examples: [`hate ${client.name} is the best bot!`],
-            cooldown: 5
-        });
-    }
+   constructor(client) {
+      super(client, {
+         name: "hate",
+         aliases: ["fuck", "allmyhomies", "homies"],
+         usage: "hate <text>",
+         description:
+            'Generates an "all my homies hate" image with provided text',
+         type: client.types.FUN,
+         examples: [`hate ${client.name} is the best bot!`],
+         cooldown: 5,
+         slashCommand: new SlashCommandBuilder()
+            .setName("hate")
+            .setDescription(
+               "Generates an 'all my homies hate' image with provided text"
+            )
+            .addStringOption((option) => {
+               return option
+                  .setName("text")
+                  .setDescription("Text to put in the image")
+                  .setRequired(true);
+            }),
+      });
+   }
 
-    async run(message, args) {
+   async interact(interaction, args) {
+      try {
+         const text = await interaction.client.utils.replaceMentionsWithNames(
+            args.join(" "),
+            interaction.guild
+         );
+         const buffer = await interaction.client.utils.generateImgFlipImage(
+            242461078,
+            `${text}`,
+            `${text}`,
+            "#EBDBD1",
+            "#2E251E"
+         );
 
+         if (buffer) {
+            const attachment = new MessageAttachment(
+               buffer,
+               "allmyhomieshate.png"
+            );
 
-        message.channel.send({embeds: [new MessageEmbed().setDescription(`${load} Loading...`)]}).then(async msg => {
+            await interaction.reply({ files: [attachment] });
+            // await msg.delete();
+         }
+      } catch (e) {
+         await interaction.reply({
+            embeds: [new MessageEmbed().setDescription(`${fail} ${e}`)],
+         });
+      }
+   }
+
+   async run(message, args) {
+      message.channel
+         .send({
+            embeds: [new MessageEmbed().setDescription(`${load} Loading...`)],
+         })
+         .then(async (msg) => {
             try {
-                const text = await message.client.utils.replaceMentionsWithNames(args.join(' '), message.guild)
-                const buffer = await msg.client.utils.generateImgFlipImage(242461078, `${text}`, `${text}`, '#EBDBD1', '#2E251E')
+               const text = await message.client.utils.replaceMentionsWithNames(
+                  args.join(" "),
+                  message.guild
+               );
+               const buffer = await msg.client.utils.generateImgFlipImage(
+                  242461078,
+                  `${text}`,
+                  `${text}`,
+                  "#EBDBD1",
+                  "#2E251E"
+               );
 
-                if (buffer) {
-                    const attachment = new MessageAttachment(buffer, "allmyhomieshate.png");
+               if (buffer) {
+                  const attachment = new MessageAttachment(
+                     buffer,
+                     "allmyhomieshate.png"
+                  );
 
-                    await message.channel.send({files: [attachment]})
-                    await msg.delete()
-                }
+                  await message.channel.send({ files: [attachment] });
+                  await msg.delete();
+               }
             } catch (e) {
-                await msg.edit({embeds: [new MessageEmbed().setDescription(`${fail} ${e}`)]})
+               await msg.edit({
+                  embeds: [new MessageEmbed().setDescription(`${fail} ${e}`)],
+               });
             }
-        })
-    }
+         });
+   }
 };
