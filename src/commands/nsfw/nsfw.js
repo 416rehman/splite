52c8f3f6a5fdbdc541b761e8c0a041a0a372f61a
Rehman <certifiedrehman@gmail.com>
2022-04-27 15:56:04 -0400

    added NSFW sample

---
+++ src/commands/nsfw/nsfw.js
@@ -0,0 +1,25 @@
+const Command = require('../Command.js');
+const {MessageEmbed} = require('discord.js');
+
+module.exports = class nsfwCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'betn',
+            usage: 'bet <user mention/id/name> <point count>',
+            aliases: ['bet'],
+            description:
+                'Bet against someone. Winner receives double the bet amount',
+            type: client.types.NSFW,
+            examples: ['betn @split 1000'],
+            exclusive: true,
+            disabled: true, // Remove this line if you want to enable the command
+        });
+    }
+
+    run(message) {
+        const embed = new MessageEmbed().setTitle('Example Command').setDescription('This is an example command.');
+        message.reply({
+            embeds: [embed]
+        });
+    }
+};
