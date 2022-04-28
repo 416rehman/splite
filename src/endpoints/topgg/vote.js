9024c399167b66925036f35e55dbcc7608a6b726
Rehman <certifiedrehman@gmail.com>
2022-04-28 02:40:52 -0400

    topgg vote endpoint

---
+++ src/endpoints/topgg/vote.js
@@ -0,0 +1,19 @@
+const Endpoint = require('../Endpoint');
+
+module.exports = class TopGGVoteEndpoint extends Endpoint {
+    constructor(webserver) {
+        super(webserver, {
+            description: 'Receives TopGG vote events.',
+            authorization: webserver.config.apiKeys.topGG.webhook_mode.authorization,
+            disabled: webserver.config.apiKeys.topGG.useMode !== 'webhook_mode',
+            method: 'POST',
+        });
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    post(req, res) {
+        if (req.body?.user) {
+            this.webserver.db.integrations.setTopGG.run(req.body.user, Date.now());
+        }
+    }
+};
