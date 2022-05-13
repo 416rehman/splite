5e566a99db439b8b26ab2f15fd27e0c920654667
416rehman <github@ahmadz.ai>
2022-05-13 16:35:27 -0400

    updated command embed
    Took 10 seconds

--- src/commands/info/vote.js
+++ src/commands/info/vote.js
@@ -19,23 +19,25 @@ module.exports = class InviteMeCommand extends Command {
             .get(message.guild.id);
         message.client.utils
             .checkTopGGVote(message.client, message.author.id)
-            .then((voted) => {
+            .then((hasVoted) => {
+                const gamblingModifier = Math.ceil((this.client.config.votePerks.gamblingWinOdds - this.client.config.stats.gambling.winOdds) * 100);
+                const robbingModifier = Math.ceil((this.client.config.votePerks.robbingSuccessOdds - this.client.config.stats.robbing.successOdds) * 100);
                 const embed = new MessageEmbed()
                     .setTitle('Vote On Top.gg')
                     .setThumbnail('https://top.gg/images/logoinverted.png')
                     .setDescription(
-                        `Click [here](https://top.gg/bot/${message.client.config.apiKeys.topGG.api_mode.id}/vote) to vote. \n\n
-                **Voting Perks**\n${emojis.Voted}**+10%** Gambling Odds - Check your odds: \`${prefix}odds\` \
-                \n*Perks will be activated 5 mins after voting*
-                `
-                    )
+                        `Click **[here](https://top.gg/bot/${message.client.config.apiKeys.topGG.api_mode.id}/vote)** to vote. \nUse the \`${prefix}odds\` command to check your odds.\n
+                        ${hasVoted ? `${emojis.Voted} Your active perks: ` : 'After voting, you will receive the following perks:'}`)
+                    .setURL('https://top.gg/bot/${message.client.config.apiKeys.topGG.api_mode.id}/vote')
+                    .addField('Gambling Odds', `${hasVoted ? emojis.Voted : ''} +${gamblingModifier}% Boost`, true)
+                    .addField('Robbing', `${hasVoted ? emojis.Voted : ''} +${robbingModifier}% Boost`, true)
                     .setFooter({
-                        text: message.member.displayName,
+                        text: `${hasVoted ? 'You have already voted, thank you <3' : 'Perks will be activated 5 mins after voting'}`,
                         iconURL: message.author.displayAvatarURL({dynamic: true}),
                     })
                     .setTimestamp()
                     .setColor(message.guild.me.displayHexColor);
-                if (!voted) {
+                if (!hasVoted) {
                     message.channel.send({embeds: [embed]});
                 }
                 else {
