9a219f386c375af48911cd35ef5bb03ab55b8cb2
416rehman <15690606+416rehman@users.noreply.github.com>
2023-02-09 11:28:08 -0500

    fix: renamed slash commands
    Took 5 minutes

--- src/commands/admin/crownRoleGroup.js
+++ src/commands/admin/crownRoleGroup.js
@@ -8,7 +8,7 @@ module.exports = class CrownSettingsCommandGroup extends Command {
             description: 'The crown role is given to the most active member of the day, and removed from all other members',
             type: client.types.ADMIN,
             userPermissions: ['ManageGuild'],
-            slashCommand: new SlashCommandBuilder()
+            slashCommand: new SlashCommandBuilder().setName('crownrole')
                 .addSubcommand((o) => o.setName('set').setDescription('Set the crown role - To view current role, don\'t provide a role').addRoleOption(p => p.setName('role').setRequired(false).setDescription('The role to set as the crown role. To view current role, don\'t provide this option')))
                 .addSubcommand((o) => o.setName('clear').setDescription('Clear the crown role')),
             subCommandMappings: {
